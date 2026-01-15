5e052bb41322a18899f9f4d3a3cd21cbd3fc548a
Shubrali Jain <shubralijain@gmail.com>
2026-01-16 00:12:42 +0530

    Initial commit

---
+++ app/(main)/ai-cover-letter/[id]/page.jsx
@@ -0,0 +1,13 @@
+///dynamic route
+import React from 'react'
+
+const CoverLetter =  ({params}) => {
+    const id =  params.id;
+    return (
+    <div>CoverLetter: {id}</div>
+  )
+}
+
+export default CoverLetter
+
+
